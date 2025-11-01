f8669a00401f45734f7109db991f449a913b100b
raj7506 <krkrajan@gmail.com>
2025-11-01 14:14:26 +0530

    Add files via upload

---
+++ Flyold_triangle.py
@@ -0,0 +1,8 @@
+print("Floyds triangle:")
+n = int(input("Enter the number of rows: "))
+num = 1
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(num,end = "")
+        num = num + 1
+    print()
